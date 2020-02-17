0cd7ef22fcbbb59667dfd9d3cf16d1a295883838
Carlo Nölle <cno@timetoact.de>
2020-02-17 23:08:30 +0100

    fix

--- gcp/project-with-vpc/outputs.tf
+++ gcp/project-with-vpc/outputs.tf
@@ -1,4 +1,4 @@
 output "project_url" {
-  value       = aws_instance.server.private_ip
+  value       = google_project.project.self_link
   description = "The URL to the project on Google Console"
 }
