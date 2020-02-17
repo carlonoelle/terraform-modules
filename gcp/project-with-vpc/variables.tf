ca686e3a77cc51da937378a138cefd7e25f3abd2
Carlo Nölle <cno@timetoact.de>
2020-02-17 23:03:16 +0100

    fix

--- gcp/project-with-vpc/variables.tf
+++ gcp/project-with-vpc/variables.tf
@@ -5,3 +5,15 @@ variable "project_id" {
 variable "project_name" {
   description = "The Name of the Google Cloud project"
 }
+
+variable "org_id" {
+  description = "The ID of the GCP organization"
+}
+
+variable "folder" {
+  description = "The folder path where to put the project"
+}
+
+variable "region" {
+  description = "The region of the subnet"
+}
